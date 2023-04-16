296951f49cc8cb6bb2bb5822420581b36f214389
Talha Choudhary <66958860+talha-choudhary@users.noreply.github.com>
2023-04-15 21:08:38 -0400

    Add files via upload

--- project/mirror.c
+++ project/mirror.c
@@ -1,126 +1,130 @@
-// Note: This code only handles the server part of the communication,
-// and does not implement the logic for the commands listed in Section 2.
-// The processclient() function needs to be further implemented to handle the
-// specific commands as required.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <sys/wait.h>
 
-#define MAX_CLIENTS 4
-#define BUFFER_SIZE 256
-int CLIENT_NO = 0;
+#define SERVER_IP "127.0.0.1" // IP address of the server
+#define SERVER_PORT 8081 // Port number of the mirror server
+#define BUFFER_SIZE 1024 // Buffer size for receiving data from clients
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
 
-void quit_handler(int sig)
-{
-    CLIENT_NO--; // Decrement count of clients
-    printf("No. of Clients Connected: %d\n", CLIENT_NO);
-}
+void processclient(int client_socket);
 
-void processclient(int client_socket)
-{
+void processclient(int client_socket) {
     char buffer[BUFFER_SIZE] = {0};
-    while (1)
-    {
+    while (1) {
         // Wait for client to send a command
         int bytes_read = recv(client_socket, buffer, sizeof(buffer), 0);
-        if (bytes_read <= 0)
-        {
+        if (bytes_read <= 0) {
+            //perror("Error: No bytes received from client mirror");
             break; // Connection closed or error
         }
         // Process command
         // Example: echo back the received command
-        printf("Got client command: %s\n", buffer);
+        printf("Client command received from mirror: %s\n",buffer);
         send(client_socket, buffer, bytes_read, 0);
         bzero(buffer, BUFFER_SIZE);
     }
     close(client_socket);
 }
 
-int main(int argc, char const *argv[])
-{
-    int portno;
-    int opt = 1;
-    signal(SIGCHLD, quit_handler);
-    // create server socket similar to what was done in client
-    int servSockD = socket(AF_INET, SOCK_STREAM, 0);
-    if (servSockD < 0)
-    {
-        error("Could not create socket");
-    }
-    // Set socket options to reuse address and port
-    if (setsockopt(servSockD, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-    {
-        error("setsockopt failed");
-    }
-    struct sockaddr_in servAddr, cliAddr;
-    // define server address
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = INADDR_ANY;
-    if (argc != 2)
-    {
-        printf("Usage: ./server portno");
+int main() {
+    int mirror_socket, client_socket, opt = 1;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUFFER_SIZE];
+    int bytes_received, addrlen = sizeof(client_addr);
+    int clients_count=0;
+
+    // Create a socket
+    mirror_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mirror_socket == -1) {
+        perror("Error: Failed to create mirror socket");
         exit(1);
     }
-    sscanf(argv[1], "%d", &portno);
-    servAddr.sin_port = htons((uint16_t)portno); // Host to network short- byte order of the port number
 
-    // bind socket to the specified IP and port
-    if (bind(servSockD, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
-    {
-        error("Bind failed");
+    // Configure server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    // Set socket options to reuse address
+    if (setsockopt(mirror_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Error: Failed to set socket options");
+        exit(1);
     }
-    printf("Listening on %d\n", portno);
 
-    // listen for connections
-    if (listen(servSockD, 5) < 0)
-    { // Max 5 connections in buffer.
-        error("listen failed");
+    // Bind the socket to the server address
+    if (bind(mirror_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Error: Failed to bind mirror socket to server address");
+        exit(1);
     }
-    int client_socket;
 
-    while (1)
-    {
-        // Accept incoming connection
-        if ((client_socket = accept(servSockD, (struct sockaddr *)&cliAddr, (socklen_t *)&cliAddr)) < 0)
-        {
-            perror("accept failed");
+    // Listen for incoming client connections
+    if (listen(mirror_socket, 4) == -1) {
+        perror("Error: Failed to listen for incoming connections");
+        exit(1);
+    }
+
+    printf("Mirror server listening on %s:%d\n", SERVER_IP, SERVER_PORT);
+
+    // Accept client connections and mirror incoming data to standard output
+    while (1) {
+        // Accept incoming client connections
+        client_socket = accept(mirror_socket, (struct sockaddr*)&client_addr, (socklen_t*)&addrlen);
+        if (client_socket == -1) {
+            perror("Error: Failed to accept incoming client connection");
             continue;
         }
 
-        // Fork child process to handle client request
+        printf("Mirror server accepted incoming client connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        // Receive data from the client and mirror it to the standard output
+        /*while ((bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+            printf("Mirror server received %d bytes from client: %s\n", bytes_received, buffer);
+            fflush(stdout); // Flush standard output buffer
+            if (send(client_socket, buffer, bytes_received, 0) == -1) {
+                perror("Error: Failed to send mirrored data to client");
+                break;
+            }
+        }*/
+
         pid_t pid = fork();
-        if (pid == -1)
-        {
+        if (pid == -1) {
             close(client_socket);
-            perror("fork failed");
+            perror("Error: Coult not fork to handle client");
             continue;
+        } else if (pid == 0) {
+        // Child process
+        printf("Client %d handled by the mirror\n", clients_count + 1);
+        close(mirror_socket); // Close unused server socket
+        processclient(client_socket);
+        exit(EXIT_SUCCESS);
+        } else {
+        clients_count++; //Increment count of clients.
+        printf("No. of Clients handled: %d\n", clients_count);
+        // Parent process
+        close(client_socket); // Close unused client socket
+        
         }
-        else if (pid == 0)
-        {
-            // Child process
-            close(servSockD); // Close unused server socket
-            processclient(client_socket);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            CLIENT_NO++; // Increment count of clients.
-            printf("No. of Clients Connected: %d\n", CLIENT_NO);
-            // Parent process
-            close(client_socket); // Close unused client socket
-        }
+
+
+        // Close the client socket
+        //close(client_socket);
+
+        printf("Mirror server closed client connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     }
-    close(servSockD);
+
+    // Close the mirror server socket
+    close(mirror_socket);
+
     return 0;
 }
+
+
+
+
+
--- project/server.c
+++ project/server.c
@@ -2,184 +2,254 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/wait.h>
 
-void handle_client(int client_socket)
-{
-    char buffer[1024];
-    ssize_t bytes_received;
+#define SERVER_IP "127.0.0.1"
+#define MAX_CLIENTS 12 // Maximum number of clients (including server and mirror)
+#define BUFFER_SIZE 1024 // Buffer size for receiving data from clients
+ FILE *fp;
+  char fileBuffer[1024] = {0};
 
-    // Receive data from the client
-    bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received == -1)
-    {
-        perror("recv");
-        close(client_socket);
-        return;
+void processclient(int client_socket);
+void handle_mirror(int client_socket);
+
+void processclient(int client_socket) {
+
+     fp = fopen("teat.zip", "rb");
+    if (fp == NULL) {
+        printf("File not found.\n");
+        //break;
     }
 
-    // Process the data
-    printf("Received %ld bytes (server): %.*s\n", bytes_received, (int)bytes_received, buffer);
+    char buffer[BUFFER_SIZE] = "In processclient()";
+    send(client_socket, buffer, 18,0);
+    bzero(buffer, BUFFER_SIZE);
+    //wait for for client
+    while (1) {
+        // int bytes_read = recv(client_socket, buffer, sizeof(buffer), 0);
+        // if (bytes_read <= 0) {
+        //     perror("Error: No bytes received from client");
+        //     break; // Connection closed or error
+        // }
+        // Process command
+        // Example: echo back the received command
+        printf("Client command received from server: %s\n",buffer);
+        //sending commend to client
+        size_t bytesRead = fread(fileBuffer, 1, sizeof(fileBuffer), fp);
+        if (bytesRead == 0) {
+            break;
+        }
+        send(client_socket, fileBuffer, bytesRead, 0);
+        bzero(buffer, BUFFER_SIZE);
+    }
+    close(client_socket);
+    fclose(fp);
+}
+/*
+void handle_mirror(int client_socket) {
+    char buffer[22] = "Mirror: 127.0.0.1 8081";// Local IP
+    //char buffer[25] = "Mirror: 192.168.2.16 8081";// Router IP
+    //Send the mirror address to client for mirror connection
+    send(client_socket, buffer, 22, 0);// 22 for local 25 for router
+    close(client_socket);
+}
+*/
 
-    // Send a message back to the client
-    const char *message = "Hello, client!";
-    ssize_t bytes_sent = send(client_socket, message, strlen(message), 0);
-    if (bytes_sent == -1)
-    {
-        perror("send");
+void handle_mirror(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    // Receive data from client
+    bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_received == -1) {
+        //perror("Error: Failed to receive data from client");
+        exit(1);
+    }
+
+    printf("Received data from client: %s\n", buffer);
+
+    // Connect to mirror server
+    int mirror_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mirror_socket == -1) {
+        perror("Error: Failed to create mirror socket");
+        exit(1);
+    }
+
+    struct sockaddr_in mirror_addr;
+    mirror_addr.sin_family = AF_INET;
+    mirror_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    mirror_addr.sin_port = htons(8081);
+
+    if (connect(mirror_socket, (struct sockaddr*)&mirror_addr, sizeof(mirror_addr)) == -1) {
+        perror("Error: Failed to connect to mirror server");
+        exit(1);
+    }
+
+    // Send data to mirror server
+    if (send(mirror_socket, buffer, bytes_received, 0) == -1) {
+        perror("Error: Failed to send data to mirror server");
+        exit(1);
+    }
+
+    // Receive data from mirror server
+    bytes_received = recv(mirror_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_received == -1) {
+        perror("Error: Failed to receive data from mirror server");
+        exit(1);
+    }
+
+    printf("Received data from mirror server: %s\n", buffer);
+
+    // Send data back to client
+    if (send(client_socket, buffer, bytes_received, 0) == -1) {
+        perror("Error: Failed to send data to client");
+        exit(1);
+    }
+
+    // Close sockets
+    close(mirror_socket);
+    close(client_socket);
+}
+
+
+int main() {
+      
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len;
+    int clients_count = 0;
+    int opt=1;
+    
+
+    // Create a socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Error: Failed to create socket");
+        exit(1);
+    }
+
+    // Set socket options to reuse address and port
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("Error: Failed to set 'setsockopt' for reusing address");
+    }
+
+    // Configure server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);//INADDR_ANY;
+    server_addr.sin_port = htons(8080); // Port number for server
+
+    // Bind the server address to the socket
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Error: Failed to bind socket");
+        exit(1);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_socket, MAX_CLIENTS) == -1) {
+        perror("Error: Failed to listen");
+        exit(1);
+    }
+
+    printf("Server is listening for incoming connections...\n");
+
+    while (1) {
+        // Accept incoming connections
+        client_addr_len = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+        if (client_socket == -1) {
+            perror("Error: Failed to accept connection");
+            exit(1);
+        }
+
+        printf("\nClient connected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        // Handle the first 4 client connections in the server
+        if (clients_count < 4 || (clients_count >= 8 && clients_count%2 == 1)) {
+            //handle_client(client_socket);
+            //clients_count++;
+            pid_t pid = fork();
+            if (pid == -1) {
+                close(client_socket);
+                perror("Error: Coult not fork to handle client");
+                continue;
+            } else if (pid == 0) {
+                // Child process
+                 printf("Client %d handled by the servor\n", clients_count + 1);
+                close(server_socket); // Close unused server socket
+                processclient(client_socket);
+                exit(EXIT_SUCCESS);
+            } else {
+                clients_count++; //Increment count of clients.
+                printf("No. of Clients handled: %d\n", clients_count);
+                // Parent process
+                close(client_socket); // Close unused client socket
+                printf("server closed client connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            }
+        }
+        
+        // Handle the next 4 client connections in the mirror
+        //else if (clients_count >= 4 && clients_count < 8) {
+        else{
+           
+            //clients_count++;
+            pid_t pid = fork();
+            if (pid == -1) {
+                close(client_socket);
+                perror("Error: Coult not fork to handle client");
+                continue;
+            } else if (pid == 0) {
+                // Child process
+                printf("Client %d handled by the mirror\n", clients_count + 1);
+                close(server_socket); // Close unused server socket
+                handle_mirror(client_socket);
+                exit(EXIT_SUCCESS);
+            } else {
+                clients_count++; //Increment count of clients.
+                printf("No. of Clients handled: %d\n", clients_count);
+                // Parent process
+                close(client_socket); // Close unused client socket
+                printf("Mirror server(server.c) closed client connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            }
+        }
+        /*
+        // Handle the remaining client connections alternately between server and mirror
+        else {
+            if (clients_count % 2 == 0) {
+                handle_client(client_socket);
+            } else {
+                printf("Client %d handled by the mirror\n", clients_count + 1);
+            }
+            clients_count++;
+        }
+        */
+    }
+
+    return 0;
+}
+/*
+void processclient(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    // Receive data from client
+    bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_received == -1) {
+        perror("Error: Failed to receive data from client");
+        exit(1);
+    }
+
+    printf("Received data from client: %s\n", buffer);
+
+    // Send data back to client
+    if (send(client_socket, buffer, bytes_received, 0) == -1) {
+        perror("Error: Failed to send data to client");
+        exit(1);
     }
 
     // Close the client socket
     close(client_socket);
 }
-
-void handle_mirror(int client_socket)
-{
-    char buffer[22] = "Mirror: 127.0.0.1 8081"; // Local IP
-    // char buffer[25] = "Mirror: 192.168.2.16 8081";// Router IP
-    // Send the mirror address to client for mirror connection
-    printf("client socket: %d", client_socket);
-    send(client_socket, buffer, 22, 0); // 22 for local 25 for router
-    close(client_socket);
-}
-
-void start_mirror_process()
-{
-    printf("Mirror Process started\n");
-    pid_t mirror_pid = fork();
-    if (mirror_pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (mirror_pid == 0)
-    {
-        if (access("./mirror", F_OK) == -1)
-        {
-            perror("access");
-            exit(EXIT_FAILURE);
-        }
-        execlp("./mirror", "./mirror", NULL); // Start the mirror process
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    }
-}
-
-int main(int argc, char *argv[])
-{
-    int server_socket, client_socket;
-    struct sockaddr_in server_address, client_address;
-    socklen_t client_address_len = sizeof(client_address);
-    int client_count = 0;
-
-    // Create a TCP socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1)
-    {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    // Bind the socket to a port
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(12345);
-    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
-    {
-        perror("bind");
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for incoming connections
-    if (listen(server_socket, 5) == -1)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Listening for clients\n");
-
-    while (1)
-    {
-        // Accept a client connection
-        client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_len);
-        if (client_socket == -1)
-        {
-            perror("accept");
-            continue;
-        }
-
-        // Handle in server for first 4
-        if (client_count < 4)
-        {
-            // Create a child process to handle the client connection
-            pid_t child_pid = fork();
-            if (child_pid == -1)
-            {
-                perror("fork");
-                close(client_socket);
-                continue;
-            }
-            else if (child_pid == 0)
-            {
-                close(server_socket);
-                handle_client(client_socket);
-                exit(EXIT_SUCCESS);
-            }
-            else
-            {
-                close(client_socket);
-                client_count++;
-                continue;
-            }
-        }
-
-        // Check if it's time to start the mirror process
-        else if (client_count >= 4 && client_count < 8)
-        {
-            // start_mirror_process();
-            handle_mirror(client_socket);
-            client_count++;
-        }
-        else
-        {
-            if (client_count % 2 == 0)
-            {
-                // Create a child process to handle the client connection
-                pid_t child_pid = fork();
-                if (child_pid == -1)
-                {
-                    perror("fork");
-                    close(client_socket);
-                    continue;
-                }
-                else if (child_pid == 0)
-                {
-                    close(server_socket);
-                    handle_client(client_socket);
-                    exit(EXIT_SUCCESS);
-                }
-                else
-                {
-                    close(client_socket);
-                    client_count++;
-                    continue;
-                }
-            }
-            else
-            {
-                // start_mirror_process();
-                handle_mirror(client_socket);
-            }
-            client_count++;
-        }
-    }
-
-    // Close the server socket
-    close(server_socket);
-
-    return 0;
-}
+*/
